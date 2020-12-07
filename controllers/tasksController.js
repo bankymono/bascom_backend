e6135b90340c1a3d1959898fdbe95e01e43b188f
Stesoda <sophiadaniel92@gmail.com>
2020-12-07 07:29:16 +0100

    bug fixes

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -33,38 +33,38 @@ const tasksController = (app) =>{
             })
     })
 
-}
-//     app.put('/tasks/:id',(req,res)=>{
-//         if(req.body.name){    
-//             connection.query(`UPDATE task SET 
-//                 name='${req.body.name}'
-//                 WHERE id=${req.params.id}`, (err,resp)=>{
-//                 if (err) throw err
-//             })
-//         }
-//         if(req.body.description){
-//             connection.query(`UPDATE tasks SET 
-//                 description='${req.body.description}'
-//                 WHERE id=${req.params.id}`, (err,resp)=>{
-//                     if (err) throw err
-//                 })
-//         }
-//         if(req.body.teamId){
-//             connection.query(`UPDATE tasks SET 
-//                 otherName='${req.body.teamId}'
-//                 WHERE id=${req.params.id}`, (err,resp)=>{
-//                     if (err) throw err
-//                 })
-//         } 
-//         res.send(" Task successfully updated")
-//     })
-    
-//     app.delete('/tasks/:id',(req,res)=>{
-//         connection.query(`DELETE FROM tasks WHERE  id=${req.params.id}`, (err,resp)=>{
-//             if (err) throw err
-//             res.send(`successfully deleted task with id ${req.params.id}`)
-//         })
-//     })
-// }
 
-module.exports = tasksController;
+    app.put('/tasks/:id',(req,res)=>{
+        if(req.body.name){    
+            connection.query(`UPDATE task SET 
+                name='${req.body.name}'
+                WHERE id=${req.params.id}`, (err,resp)=>{
+                if (err) throw err
+            })
+        }
+        if(req.body.description){
+            connection.query(`UPDATE tasks SET 
+                description='${req.body.description}'
+                WHERE id=${req.params.id}`, (err,resp)=>{
+                    if (err) throw err
+                })
+        }
+        // if(req.body.teamId){
+        //     connection.query(`UPDATE tasks SET 
+        //         otherName='${req.body.teamId}'
+        //         WHERE id=${req.params.id}`, (err,resp)=>{
+        //             if (err) throw err
+        //         })
+        // } 
+        res.send(" Task successfully updated")
+    })
+    
+    app.delete('/tasks/:id',(req,res)=>{
+        connection.query(`DELETE FROM tasks WHERE  id=${req.params.id}`, (err,resp)=>{
+            if (err) throw err
+            res.send(`successfully deleted task with id ${req.params.id}`)
+        })
+    })
+}
+
+module.exports = tasksController
