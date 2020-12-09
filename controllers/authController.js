fb87108993a54839959324a04a20b4e819df7274
bankymono <bankymono@gmail.com>
2020-12-09 14:53:06 +0100

    Updated the project controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,8 +28,8 @@ const can = (action, data) => {
   };
 
 // Check Permission to View User
-const viewUser = (req, res, next) => {
-  if (can("view_user", req.user.data.permissions) === true) {
+const viewAllUsers = (req, res, next) => {
+  if (can("view_all_users", req.user.data.permissions) === true) {
     next();
   } else {
     res.status(403).send("You don't have permission to view all users");
@@ -54,69 +54,69 @@ const deleteUser = (req, res, next) => {
     }
 };
 
-const createProject = (req, res, next) => {
-    if (can("create_project", req.user.data.permissions) === true) {
+// const createProject = (req, res, next) => {
+//     if (can("create_project", req.user.data.permissions) === true) {
+//       next();
+//     } else {
+//       res.status(403).send("You are not authorized to do so!");
+//     }
+// };
+
+const viewAllProjects = (req, res, next) => {
+    if (can("view_all_projects", req.user.data.permissions) === true) {
       next();
     } else {
       res.status(403).send("You are not authorized to do so!");
     }
 };
 
-const viewProject = (req, res, next) => {
-    if (can("view_project", req.user.data.permissions) === true) {
+// const editProject = (req, res, next) => {
+//     if (can("edit_project", req.user.data.permissions) === true) {
+//       next();
+//     } else {
+//       res.status(403).send("You are not authorized to do so!");
+//     }
+// };
+
+// const deleteProject = (req, res, next) => {
+//     if (can("delete_project", req.user.data.permissions) === true) {
+//       next();
+//     } else {
+//       res.status(403).send("You are not authorized to do so!");
+//     }
+// };
+
+// const createTask = (req, res, next) => {
+//     if (can("create_task", req.user.data.permissions) === true) {
+//       next();
+//     } else {
+//       res.status(403).send("You are not authorized to do so!");
+//     }
+// };
+
+const viewAllTasks = (req, res, next) => {
+    if (can("view_all_tasks", req.user.data.permissions) === true) {
       next();
     } else {
       res.status(403).send("You are not authorized to do so!");
     }
 };
 
-const editProject = (req, res, next) => {
-    if (can("edit_project", req.user.data.permissions) === true) {
-      next();
-    } else {
-      res.status(403).send("You are not authorized to do so!");
-    }
-};
+// const editTask = (req, res, next) => {
+//     if (can("edit_task", req.user.data.permissions) === true) {
+//       next();
+//     } else {
+//       res.status(403).send("You are not authorized to do so!");
+//     }
+// };
 
-const deleteProject = (req, res, next) => {
-    if (can("delete_project", req.user.data.permissions) === true) {
-      next();
-    } else {
-      res.status(403).send("You are not authorized to do so!");
-    }
-};
-
-const createTask = (req, res, next) => {
-    if (can("create_task", req.user.data.permissions) === true) {
-      next();
-    } else {
-      res.status(403).send("You are not authorized to do so!");
-    }
-};
-
-const viewTask = (req, res, next) => {
-    if (can("view_task", req.user.data.permissions) === true) {
-      next();
-    } else {
-      res.status(403).send("You are not authorized to do so!");
-    }
-};
-
-const editTask = (req, res, next) => {
-    if (can("edit_task", req.user.data.permissions) === true) {
-      next();
-    } else {
-      res.status(403).send("You are not authorized to do so!");
-    }
-};
-
-const deleteTask = (req, res, next) => {
-    if (can("delete_task", req.user.data.permissions) === true) {
-      next();
-    } else {
-      res.status(403).send("You are not authorized to do so!");
-    }
-};
+// const deleteTask = (req, res, next) => {
+//     if (can("delete_task", req.user.data.permissions) === true) {
+//       next();
+//     } else {
+//       res.status(403).send("You are not authorized to do so!");
+//     }
+// };
 
 
 
@@ -124,14 +124,14 @@ const deleteTask = (req, res, next) => {
 // Export modules
 module.exports.authenticate = authenticate;
 module.exports.addUser = addUser;
-module.exports.viewUser = viewUser;
+module.exports.viewAllUsers = viewAllUsers;
 module.exports.editUser = editUser;
 module.exports.deleteUser = deleteUser;
-module.exports.createProject = createProject;
+// module.exports.createProject = createProject;
 module.exports.editProject = editProject;
-module.exports.viewProject = viewProject;
-module.exports.deleteProject = deleteProject;
-module.exports.createTask = createTask;
-module.exports.editTask = editTask;
-module.exports.deleteTask = deleteTask;
-module.exports.viewTask = viewTask;
+module.exports.viewAllProjects = viewAllProjects;
+// module.exports.deleteProject = deleteProject;
+// module.exports.createTask = createTask;
+// module.exports.editTask = editTask;
+// module.exports.deleteTask = deleteTask;
+module.exports.viewAllTasks = viewAllTasks;
