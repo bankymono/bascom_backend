f44103291237d249d6f149054c63708c0d1a3bca
bankymono <bankymono@gmail.com>
2021-01-07 10:56:50 +0100

    another fix 3

--- models/db.js
+++ models/db.js
@@ -52,10 +52,10 @@ require("dotenv").config();
 
 const mysql = require('mysql')
 var connection = mysql.createPool({
-  host:process.env.DATABASE_HOST,
-  user:process.env.DATABASE_USERNAME,
-  password:process.env.DATABASE_PASSWORD, 
-  database:process.env.DATABASE
+  host:process.env.REMOTE_DB_HOST,
+  user:process.env.REMOTE_DB_USERNAME,
+  password:process.env.REMOTE_DB_PASSWORD, 
+  database:process.env.REMOTE_DB_DATABASE
 })
 
 // connection.connect((err,res)=>{
