d8f2aaa6f87e35d99bf941b5f157118c4322d180
bankymono <bankymono@gmail.com>
2021-01-12 02:11:48 +0100

    get projects, get single Project updated

--- routes/projects.route.js
+++ routes/projects.route.js
@@ -5,13 +5,16 @@ const auth = require("../controllers/authController");
 
 
 router.get('/all', auth.authenticate,auth.viewAllProjects,projectsController.getAllProjects)
-router.get('/usrprojs', auth.authenticate, projectsController.getProjects)
-router.get('/usrprojs/:projectId', auth.authenticate, projectsController.getSingleProject)
-router.put('/usrprojs/:projectId/addteam', auth.authenticate, projectsController.addTeam)
+
+router.get('/userprojects', auth.authenticate, projectsController.getProjects)
+router.get('/userprojects/:projectId', auth.authenticate, projectsController.getSingleProject)
+
+router.post('/userprojects/:projectId/addteam', auth.authenticate, projectsController.addTeam)
+
 // router.get('/:projectId/tasks', auth.authenticate, projectsController.getProjectTasks)
 // router.post('/:projectId/tasks', auth.authenticate, projectsController.createProjectTask)
-router.post('/usrprojs', auth.authenticate, projectsController.createProject)
-router.put('/usrprojs/:projectId', auth.authenticate, projectsController.updateProject)
-router.delete('/usrprojs/:projectId', auth.authenticate, projectsController.deleteProject)
+router.post('/userprojects/create', auth.authenticate, projectsController.createProject)
+router.put('/userprojects/:projectId', auth.authenticate, projectsController.updateProject)
+router.delete('/userprojects/:projectId', auth.authenticate, projectsController.deleteProject)
 
 module.exports = router
